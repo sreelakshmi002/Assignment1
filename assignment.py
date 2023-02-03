4dc7fd495c8f4fc7b82669254262002206021c00
sreelakshmi002 <124381399+sreelakshmi002@users.noreply.github.com>
2023-02-03 21:30:53 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,4 @@
+import random
+messages = ["Today will be a lucky day", "You will meet someone new and interesting", "Watch out for obstacles in your path"]
+number = random.randint(0,2)
+print(messages[number])
